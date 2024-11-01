88edcd7504e1d518c22b9edf9272e5116ae8036d
Nanskip <nanskip.oof@gmail.com>
2024-11-01 21:49:16 +0200

    fix

--- modules/player.lua
+++ modules/player.lua
@@ -13,7 +13,7 @@ player.spawn = function(self)
     Player:SetParent(World)
     Player.Tick = function(s)
         s.Position.Y = 0.5
-        local dir = rotate45(player.joystick:getValues().X, player.joystick:getValues().Y)
+        local dir = rotate45({player.joystick:getValues().X, player.joystick:getValues().Y})
         s.Velocity = Number3(dir[1], 0, dir[2])*10
 
         Camera.Position = Player.Position + Number3(-50, 60, -50)
