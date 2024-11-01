e429a656f98f4e04a2f621289d25c03f892449c8
Nanskip <nanskip.oof@gmail.com>
2024-11-02 01:57:39 +0200

    hm

--- modules/interface.lua
+++ modules/interface.lua
@@ -20,8 +20,16 @@ function interface.CREATE(self)
         Color(255, 255, 255),
         Color(255, 255, 255),
     }
-    self.etc.screen_left.Color = { gradient="H", from=Color(255, 255, 255), to=Color(255, 255, 255)}
     self.etc.screen_right = ui:createFrame()
+    self.etc.rightColors = {
+        Color(255, 255, 255),
+        Color(255, 255, 255),
+        Color(255, 255, 255),
+    }
+
+    self.Tick = LocalEvent:Listen(LocalEvent.Name.Tick, function(dt)
+        interface:UPDATE_SIDES()
+    end)
 
     self:UPDATE()
 end
@@ -37,7 +45,9 @@ function interface.UPDATE(self)
         player.joystick:setScale(3*SCREEN_MUL)
         player.joystick:setPos(Number2(Screen.Width/2 - player.joystick.shape.Width/2, Screen.Height/6 - player.joystick.shape.Height/2))
     end
+end
 
+function interface.UPDATE_SIDES(self)
     for i=-1, 1 do
         local ray = Ray(Camera.Position, Camera.Forward + Camera.Left*0.1 + Camera.Up*i*0.1)
         local impact = ray:Cast({1, 2})
@@ -46,6 +56,12 @@ function interface.UPDATE(self)
                 math.floor(lerp(self.etc.leftColors[i+2].R, impact.Block.Color.R, 0.1)),
                 math.floor(lerp(self.etc.leftColors[i+2].G, impact.Block.Color.G, 0.1)),
                 math.floor(lerp(self.etc.leftColors[i+2].B, impact.Block.Color.B, 0.1))
+            )
+        elseif impact.Object.Color ~= nil then
+            self.etc.leftColors[i+2] = Color(
+                math.floor(lerp(self.etc.leftColors[i+2].R, impact.Object.Color.R, 0.1)),
+                math.floor(lerp(self.etc.leftColors[i+2].G, impact.Object.Color.G, 0.1)),
+                math.floor(lerp(self.etc.leftColors[i+2].B, impact.Object.Color.B, 0.1))
             )
         end
     end
@@ -56,7 +72,34 @@ function interface.UPDATE(self)
         math.floor((self.etc.leftColors[1].B + self.etc.leftColors[2].B + self.etc.leftColors[3].B)/3)
     )
     self.etc.screen_left.Color = { gradient="H", from=Color(68, 68, 68), to=leftColor}
-    self.etc.screen_left.Width, self.etc.screen_left.Height = 100, 100
+    self.etc.screen_left.Width, self.etc.screen_left.Height = (Screen.Width-Screen.Height*0.5625)/2, Screen.Height
+
+    for i=-1, 1 do
+        local ray = Ray(Camera.Position, Camera.Forward + Camera.Right*0.1 + Camera.Up*i*0.1)
+        local impact = ray:Cast({1, 2})
+        if impact.Block.Color ~= nil then
+            self.etc.rightColors[i+2] = Color(
+                math.floor(lerp(self.etc.rightColors[i+2].R, impact.Block.Color.R, 0.1)),
+                math.floor(lerp(self.etc.rightColors[i+2].G, impact.Block.Color.G, 0.1)),
+                math.floor(lerp(self.etc.rightColors[i+2].B, impact.Block.Color.B, 0.1))
+            )
+        elseif impact.Object.Color ~= nil then
+            self.etc.rightColors[i+2] = Color(
+                math.floor(lerp(self.etc.rightColors[i+2].R, impact.Object.Color.R, 0.1)),
+                math.floor(lerp(self.etc.rightColors[i+2].G, impact.Object.Color.G, 0.1)),
+                math.floor(lerp(self.etc.rightColors[i+2].B, impact.Object.Color.B, 0.1))
+            )
+        end
+    end
+
+    local rightColor = Color(
+        math.floor((self.etc.rightColors[1].R + self.etc.rightColors[2].R + self.etc.rightColors[3].R)/3),
+        math.floor((self.etc.rightColors[1].G + self.etc.rightColors[2].G + self.etc.rightColors[3].G)/3),
+        math.floor((self.etc.rightColors[1].B + self.etc.rightColors[2].B + self.etc.rightColors[3].B)/3)
+    )
+    self.etc.screen_right.Color = { gradient="H", from=rightColor, to=Color(68, 68, 68)}
+    self.etc.screen_right.Width, self.etc.screen_right.Height = (Screen.Width-Screen.Height*0.5625)/2, Screen.Height
+    self.etc.screen_right.pos = Number2(Screen.Width - self.etc.screen_right.Width)
 end
 
 return interface
