4d87e909422eb2799603a6a0ebee32806db525eb
Nanskip <nanskip.oof@gmail.com>
2024-11-01 15:43:03 +0200

    fixed movement

--- modules/player.lua
+++ modules/player.lua
@@ -3,6 +3,8 @@ local player = {}
 player.INIT = function(self)
     self.joystick = joysticks.create()
     self.joystick:setPos(Number2(Screen.Width / 2, Screen.Height / 2))
+    Camera:SetModeFree()
+    Camera.Rotation = Number3(math.pi/4, math.pi/4, 0)
 
     return true
 end
@@ -11,9 +13,9 @@ player.spawn = function(self)
     Player:SetParent(World)
     Player.Tick = function(s)
         s.Position.Y = 0.5
+        s.Velocity.Y = Number3(player.joystick:getValues()[1], 0, player.joystick:getValues()[2])
 
-        s.Position.X = s.Position.X + self.joystick:getValues()[1]
-        s.Position.Z = s.Position.Z + self.joystick:getValues()[2]
+        Camera.Position = Player.Position + Number3(-50, 60, -50)
     end
 end
 
