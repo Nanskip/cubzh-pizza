c21046912e98bae385b693e30efb02cd32f215de
Nanskip <nanskip.oof@gmail.com>
2024-11-01 15:35:46 +0200

    testing player

--- modules/player.lua
+++ modules/player.lua
@@ -1,7 +1,20 @@
 local player = {}
 
 player.INIT = function(self)
+    self.joystick = joysticks.create()
+    self.joystick:setPos(Screen.Width / 2, Screen.Height / 2)
+
     return true
+end
+
+player.spawn = function(self)
+    Player:SetParent(World)
+    Player.Tick = function(s)
+        s.Position.Y = 0.5
+
+        s.Position.X = s.Position.X + self.joystick:getValues()[1]
+        s.Position.Z = s.Position.Z + self.joystick:getValues()[2]
+    end
 end
 
 return player
